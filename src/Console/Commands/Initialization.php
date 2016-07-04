8ddbc200638169c2e474516bece2357235129606
samuel kamau <sam@ralphowino.com>
2016-07-04 15:27:01 +0300

    Change to Unix stlye directory seperator

--- src/Console/Commands/Initialization.php
+++ src/Console/Commands/Initialization.php
@@ -3,8 +3,8 @@
 namespace Ralphowino\ApiStarter\Console\Commands;
 
 use Illuminate\Console\Command;
-use Ralphowino\ApiStarter\Console\Traits\BuildClassTrait;
 use Ralphowino\ApiStarter\Console\Traits\FileWizard;
+use Ralphowino\ApiStarter\Console\Traits\BuildClassTrait;
 use Ralphowino\ApiStarter\Console\Traits\ProcessRunnerTrait;
 
 class Initialization extends Command
@@ -67,7 +67,7 @@ class Initialization extends Command
     public function addRoutes()
     {
         //Get the routes template content
-        $content = \File::get($this->package_path('Stubs\\routes.blade.php'));
+        $content = \File::get($this->package_path('Stubs/routes.blade.php'));
         //Append to the routes.php file
         $this->addToFile('routes.php', 'routes', "\n\n" . $content);
     }
@@ -81,11 +81,11 @@ class Initialization extends Command
     public function publishAllFiles()
     {
         $this->publishes([
-            $this->package_path('Controllers') => app_path('Http\\Controllers\\Api'),
-            $this->package_path('Data\\Models') => app_path('Data\\Models'),
-            $this->package_path('Data\\Repositories') => app_path('Data\\Repositories'),
-            $this->package_path('Templates\\init-middleware.php') => app_path('Http\\Kernel.php'),
-            $this->package_path('config\\view.php') => base_path('config\\view.php'),
+            $this->package_path('Controllers') => app_path('Http/Controllers/Api'),
+            $this->package_path('Data/Models') => app_path('Data/Models'),
+            $this->package_path('Data/Repositories') => app_path('Data/Repositories'),
+            $this->package_path('Templates/init-middleware.php') => app_path('Http/Kernel.php'),
+            $this->package_path('config/view.php') => base_path('config/view.php'),
         ]);
     }
 
@@ -96,7 +96,7 @@ class Initialization extends Command
      */
     public function addToBaseController()
     {
-        $content = \File::get($this->package_path('Templates\\base-controller.php'));
-        $this->writeToBottomOfClass(app_path('Http\\Controllers\\Controller.php'), $content);
+        $content = \File::get($this->package_path('Templates/base-controller.php'));
+        $this->writeToBottomOfClass(app_path('Http/Controllers/Controller.php'), $content);
     }
 }
